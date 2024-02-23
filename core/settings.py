e9fced4bc3b53240d1a60aa954e221ac00e2dfc2
Dottenk <martinrojas.marv@gmail.com>
2024-02-23 13:42:26 -0300

    cambios

--- core/settings.py
+++ core/settings.py
@@ -167,4 +167,5 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/admin/logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
