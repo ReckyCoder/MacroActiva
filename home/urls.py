9c7ab77578491455dbb0b07135a04095b0be8849
Flipe <66888782+Pipatzo@users.noreply.github.com>
2024-02-27 17:55:04 -0300

    Avance Inventario
    Primer avance de inventario

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('inventario', views.inventario, name='inventario'),
+    path('ingenieria', views.ingenieria, name='ingenieria'),
+    path('procesos', views.procesos, name='procesos'),
+    path('produccion', views.produccion, name='produccion'),
+    path('logistica', views.logistica, name='logistica'),
+    path('mantenimiento', views.mantenimiento, name='mantenimiento'),
+    path('seguridad', views.seguridad, name='seguridad')
 ]
--- home/views.py
+++ home/views.py
@@ -12,3 +12,59 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/dashboard.html', context=context)
+
+def inventario(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/inventario.html', context=context)
+
+def ingenieria(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/ingenieria.html', context=context)
+
+def procesos(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/procesos.html', context=context)
+
+def produccion(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = "Producción"
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/produccion.html', context=context)
+
+def logistica(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/logistica.html', context=context)
+
+def mantenimiento(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/mantenimiento.html', context=context)
+
+def seguridad(request):
+    #AGREGAR EN CADA VIEW PARA NAVIGATION
+    modulo = "Producción"
+    nombre_vista = request.resolver_match.url_name
+    context = {'modulo': modulo, 'nombre_vista': nombre_vista}
+    
+    return render(request, 'pages/Produccion/seguridad.html', context=context)
