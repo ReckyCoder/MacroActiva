3d6fedf14d8174cdf88c83a0fbfcbc7bccf7ce52
Dottenk <martinrojas.marv@gmail.com>
2024-02-27 17:43:42 -0300

    xd

--- apps/rh/empleado/models.py
+++ apps/rh/empleado/models.py
@@ -7,14 +7,14 @@ from apps.rh.empleado.models import *
 
 class Cargo(models.Model):
     nombre  = models.CharField(max_length=50)
-    perfil = models.CharField(max_field=3000)
+    perfil = models.CharField(max_length=3000)
 
     def __str__(self):
         return self.nombre
 
 class Empleado(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
-    cargo = models.ForeignKey(Cargo, on_delete=models.PROTECT)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.usuario.__getattribute__("username")
