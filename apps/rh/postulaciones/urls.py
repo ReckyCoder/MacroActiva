37d6515b1b72d2ef0fc80a0ede2e26949666efb6
Dottenk <martinrojas.marv@gmail.com>
2024-03-22 22:32:33 -0300

    Se añade, de forma incompleta el modulo de postulaciones

--- apps/rh/postulaciones/urls.py
+++ apps/rh/postulaciones/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('menu/', views.index, name='menupostulaciones'),
+    path('menu/gestionarPostulaciones/', views.gestionarPostulaciones, name='rhGestionarPostulaciones')
 ]
