5fd722de69bf7d9446144f4de9c606a449eb4be4
Dottenk <martinrojas.marv@gmail.com>
2024-04-01 12:44:36 -0300

    xd

---
+++ static/js/crmventas/postventa.js
@@ -0,0 +1,8 @@
+function cambiarEstado(nuevoEstado) {
+    // Cambiar el texto del botón "Estado" con el nuevo estado seleccionado
+    document.getElementById('estadoDropdown').innerText = nuevoEstado;
+}
+
+
+
+
